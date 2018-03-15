9727685a57a4057011ed48417cd0d7adaf175a2d
lucky <945025184@qq.com>
2018-03-15 14:12:05 +0800

    huoqu

--- text1.php
+++ text1.php
@@ -1,21 +1,5 @@
 <?php
 
-function order($arr){
-	$length = count($arr);
-	for($j = 0; $j < $length; $j++) {
-		for($i = 0; $i < $length-$j-1; $i++) {
-			if($arr[$i] > $arr[$i+1]) {
-				$temp = $arr[$i];
-				$arr[$i] = $arr[$i+1];
-				$arr[$i+1] = $temp;
-			}
-		}
-	}
-	return $arr;
-}
 
-$arr = [1,5,8,9,3,88,44,22,99,7,4,1];
-$ord = order($arr);
-print_r($ord);
 
 ?>
