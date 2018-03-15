ea86f643ed090f1be3fa778f3493b6c289b564d5
lucky <945025184@qq.com>
2018-03-15 19:37:56 +0800

    99乘法表

--- test2.php
+++ test2.php
@@ -1,5 +1,17 @@
 <?php
-echo '哈哈哈更新';
+for($i = 9; $i >= 1; $i--) {
+	for($j = $i; $j >= 1; $j--) {
+		echo "{$i}X{$j}=".$i*$j.' ';
+	}
+	echo '<br />';
+}
+
+
+for($i = 1; $i <= 9; $i++) {
+	for($j = 1; $j <=$i; $j++) {
+		echo "{$j}X{$i}=".$i*$j.' ';
+	}
+	echo '<br />';
+}
 
 ?>
-asdf
