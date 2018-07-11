d8b6284bd2d35d939f7d254b0486afe86921c6da
lyl <945025184@qq.com>
2018-07-12 00:06:21 +0800

    delete

--- forum/app/Forumimage.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App;
-use Illuminate\Database\Eloquent\Model;
-class Forumimage extends Model
-{
-    protected $table = 'forumimage';
-    public $timestamps = false;
-    protected $fillable = ['forum_id','image'];
-}
